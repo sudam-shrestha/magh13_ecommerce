6e3ff3acd83cc905a6dbfe6577f948027df89155
Sudam939 <sudamshrestha939@gmail.com>
2025-02-24 21:28:15 +0545

    first commit

---
+++ app/Filament/Resources/CompanyResource.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyResource\Pages;
+use App\Filament\Resources\CompanyResource\RelationManagers;
+use App\Filament\Resources\CompanyResource\RelationManagers\ClientsRelationManager;
+use App\Models\Company;
+use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CompanyResource extends Resource
+{
+    protected static ?string $model = Company::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office';
+    protected static ?string $modelLabel = 'Company';
+    protected static ?string $pluralModelLabel = 'Company';
+    protected static ?string $navigationGroup = 'Settings';
+    protected static ?int $navigationSort = 1;
+
+    public static function canCreate(): bool
+    {
+        return Company::count() == 0 ? true : false;
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make("Company Information")
+                    ->columns(2)
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                            ->email()
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TagsInput::make('phone')
+                            ->placeholder('Enter phone number')
+                            ->required(),
+                        Forms\Components\TextInput::make('address')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\RichEditor::make('terms')
+                            ->label("Terms and Conditions")
+                            ->columnSpanFull()
+                            ->default(null),
+                        Forms\Components\RichEditor::make('privacy')
+                            ->label("Privacy Policy")
+                            ->columnSpanFull()
+                            ->default(null),
+                        Forms\Components\FileUpload::make('logo')
+                            ->maxSize(1024)
+                            ->directory('logos')
+                            ->required(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('logo')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanies::route('/'),
+            'create' => Pages\CreateCompany::route('/create'),
+            'edit' => Pages\EditCompany::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CompanyResource/Pages/CreateCompany.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\CompanyResource\Pages;
+
+use App\Filament\Resources\CompanyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCompany extends CreateRecord
+{
+    protected static string $resource = CompanyResource::class;
+
+    public static function canCreateAnother(): bool
+    {
+        return false;
+    }
+}
